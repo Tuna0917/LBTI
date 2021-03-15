531b4d7e0c9f93c2b244e33fd3703f58554542c6
Tuna0917 <trick@kaist.ac.kr>
2021-03-15 17:54:10 +0900

    1312313131321

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
-from flask import Flask, render_template, request
-from questions import *
-from alignments import alignment
-
+from flask import Flask, render_template, request, url_for, redirect
+from characters import champions
+from quest import QnA, champ_list
 app = Flask(__name__)
-line_page = 1
+
 @app.route("/")
 def home():
 
     return render_template("home.html")
 
-@app.route("/<int:page>/")
-def line_question(page:int):
-    if page>line_page:
-        return render_template('404.html')
+@app.route("/<int:q>/<int:page>/")
+def question(page:int, q:int):
+    if 2**(q-1)> page or page >= 2**q:
+        return redirect(url_for('home'))
+    if q==5:
+        return redirect(url_for('result', champ=champ_list[page - 16]))
+    if q>5 or q<0:
+        return redirect(url_for('home'))
     return render_template(
         "question.html", 
         template_page = page, 
-        template_line = False,
-        template_question=question_list[page], 
-        template_answers = line_list,
+        template_question=QnA[q], 
+        template_q = q+1
         )
-
-@app.route("/<int:page>/<line>/")
-def champ_question(page:int, line:str):
-    que = []
-    if line == 'top':
-        que = top_list
-    elif line == 'jg':
-        que = jung_list
-    elif line == 'mid':
-        que = mid_list
-    elif line == 'adc':
-        que = adc_list
-    elif line == 'sup':
-        que = sup_list
-    else:
-        return render_template('404.html')
-    return render_template('question.html', template_page = page, template_line = line, template_question=question_list[page], template_answers = que)
 
 @app.route("/result/<champ>/")
 def result(champ:str):
-    if champ in alignment:
-        return render_template("result.html", alignment = alignment[champ])
-    else:
-        return render_template('404.html')
+    return render_template("result.html", alignment = champions[champ])
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- characters.py
+++ characters.py
@@ -1,4 +1,4 @@
-alignment = { 
+champions = { 
     "Nasus" : ["나서스","https://ifh.cc/g/rycEAT.jpg", "Nasus", "올해도 풍작이군!", "왕귀형 챔프의 대표주자"], 
     "Ornn" : ["오른","https://ifh.cc/g/OHDdGj.jpg", "Ornn", "이겨쓰! 이겨쓰! 이겨쓰!", "부연설명"], 
     "Camille" : ["카밀","https://ifh.cc/g/83BUnd.jpg", "Camille", "게임의 주인공은 나야!", "부연설명"],
@@ -11,7 +11,7 @@ alignment = {
     "Xerath" : ["제라스", "https://ifh.cc/g/7Aouzd.jpg", "Xerath", "포킹의 정석", "부연설명"], 
     "Zed" : ["제드", "https://ifh.cc/g/1okQ4e.jpg", "Zed", "목격자만 없으면 암살이죠", "부연설명"], 
     "Yasuo" : ["야스오", "https://ifh.cc/g/MCsHoF.jpg", "Yasuo", "저는 이과가 아닙니다.", "부연설명"], 
-    "Galio" : ["갈리오", "https://ifh.cc/g/LRQkQF.jpg", "Galio", "나. 강림.", "부연설명"], 
+    "Galio" : ["갈리오", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Galio_0.jpg", "Galio", "나. 강림.", "부연설명"], 
 
     "Jinx" : ["징크스", "https://ifh.cc/g/VgXGDR.jpg", "Jinx", "신난다!!!!!!!!", "부연설명"], 
     "Kogmaw" : ["코그모", "https://ifh.cc/g/kjRlfU.jpg", "KogMaw", "퉤퉤퉤퉤퉤퉤퉤퉤", "부연설명"], 
@@ -21,4 +21,9 @@ alignment = {
     "Brand" : ["브랜드", "https://ifh.cc/g/toyi0Z.jpg", "Brand", "원딜은 쉬고있어!", "부연설명"], 
     "Janna" : ["잔나", "https://ifh.cc/g/vFgrki.jpg", "Janna", "시팅", "부연설명"], 
     "Bard" : ["바드", "https://ifh.cc/g/kRnasc.jpg", "Bard", "동에 번쩍 서에 번쩍", "부연설명"], 
+
+    "Kaisa" : ["카이사", "https://ifh.cc/g/4YFxPm.jpg", "Kaisa", "강하다", "부연설명"],
+    "Leona" : ["레오나", "https://ifh.cc/g/XSytqg.jpg", "Leona", "돌격!", "부연설명"],
+    "Irelia" : ["이렐리아", "https://ifh.cc/g/eqZOUK.jpg", "Irelia", "브루저의 대명사", "부연설명"],
+    "Jhin" : ["진", "https://ifh.cc/g/mDfYzO.jpg", "Jhin", "땅땅땅빵!", "부연설명"]
     }
