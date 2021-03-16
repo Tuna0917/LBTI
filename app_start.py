0b8fafd198b2d4ab3a5e4c6fc4bf27b3ed59d088
Tuna0917 <trick@kaist.ac.kr>
2021-03-16 17:20:08 +0900

    10

--- app_start.py
+++ app_start.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 from characters import champions
-from quest import QnA, champ_list
+from quest import QnA, champ_list, texts
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,24 +8,32 @@ def home():
 
     return render_template("home.html")
 
-@app.route("/<int:q>/<int:page>/")
-def question(page, q):
+@app.route("/<int:q>/<int:page>/<int:answer>")
+def question(page, q, answer):
     if 2**(q-1)> page or page >= 2**q:
         return redirect(url_for('home'))
     if q==5:
-        return redirect(url_for('result', champ=champ_list[page - 16]))
+        
+        return redirect(url_for('result', champ=champ_list[page - 16], answer = answer))
     if q>5 or q<0:
         return redirect(url_for('home'))
     return render_template(
         "question.html", 
         template_page = page, 
-        template_question=QnA[q], 
-        template_q = q+1
+        template_question=QnA[q-1], 
+        template_q = q+1,
+        template_answer = answer
         )
 
-@app.route("/result/<champ>/")
-def result(champ):
-    return render_template("result.html", alignment = champions[champ])
+@app.route("/result/<champ>/<int:answer>")
+def result(champ,answer):
+    answer = '000'+str(answer)
+    ans = ''
+    for i,x in enumerate(reversed(answer)):
+        ans += texts[i][int(x)]
+        if i ==3:
+            break
+    return render_template("result.html", alignment = champions[champ],answer=ans)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- characters.py
+++ characters.py
@@ -1,29 +1,22 @@
 champions = { 
-    "Nasus" : ["나서스","https://ifh.cc/g/rycEAT.jpg", "Nasus", "올해도 풍작이군!", "왕귀형 챔프의 대표주자"], 
-    "Ornn" : ["오른","https://ifh.cc/g/OHDdGj.jpg", "Ornn", "이겨쓰! 이겨쓰! 이겨쓰!", "부연설명"], 
-    "Camille" : ["카밀","https://ifh.cc/g/83BUnd.jpg", "Camille", "게임의 주인공은 나야!", "부연설명"],
-    "Teemo" : ["티모", "https://ifh.cc/g/fMXPMg.jpg", "Teemo", "하하하!", "부연설명"],
+    "Nasus" : ["나서스","https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Nasus_0.jpg", "Nasus", "나서스, 사막의 관리자", "왕귀형 챔프의 대표주자"], 
+    "Ornn" : ["오른","https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ornn_0.jpg", "Ornn", "오른, 거산의 화염", "부연설명"], 
+    "LeeSin" : ["리 신","https://ddragon.leagueoflegends.com/cdn/img/champion/splash/LeeSin_0.jpg", "LeeSin", "리신, 눈 먼 수도승", "부연설명"], 
+    "Nocturne" : ["녹턴", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Nocturne_0.jpg", "Nocturne", "녹턴, 영원한 악몽", "부연설명"], 
+    "Amumu" : ["아무무", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Amumu_0.jpg", "Amumu", "아무무, 슬픈 미라", "부연설명"], 
 
-    "LeeSin" : ["리 신","https://ifh.cc/g/2vd3DC.jpg", "LeeSin", "어디로 가야하오...", "부연설명"], 
-    "Nocturne" : ["녹턴", "https://ifh.cc/g/HwIT3u.jpg", "Nocturne", "혼자 남았구나...", "부연설명"], 
-    "Amumu" : ["아무무", "https://ifh.cc/g/q5TsM2.jpg", "Amumu", "코끼리는 똥을 푸짐하게 싸요", "부연설명"], 
-
-    "Xerath" : ["제라스", "https://ifh.cc/g/7Aouzd.jpg", "Xerath", "포킹의 정석", "부연설명"], 
-    "Zed" : ["제드", "https://ifh.cc/g/1okQ4e.jpg", "Zed", "목격자만 없으면 암살이죠", "부연설명"], 
-    "Yasuo" : ["야스오", "https://ifh.cc/g/MCsHoF.jpg", "Yasuo", "저는 이과가 아닙니다.", "부연설명"], 
+    "Xerath" : ["제라스", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Xerath_0.jpg", "Xerath", "제라스, 초월한 암살자", "부연설명"], 
+    "Zed" : ["제드", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zed_0.jpg", "Zed", "", "부연설명"], 
     "Galio" : ["갈리오", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Galio_0.jpg", "Galio", "나. 강림.", "부연설명"], 
 
-    "Jinx" : ["징크스", "https://ifh.cc/g/VgXGDR.jpg", "Jinx", "신난다!!!!!!!!", "부연설명"], 
-    "Kogmaw" : ["코그모", "https://ifh.cc/g/kjRlfU.jpg", "KogMaw", "퉤퉤퉤퉤퉤퉤퉤퉤", "부연설명"], 
-    "Ezreal" : ["이즈리얼", "https://ifh.cc/g/LcNJxN.jpg", "Ezreal", "많고많은~", "부연설명"], 
+    "Jinx" : ["징크스", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_0.jpg", "Jinx", "징크스, 난폭한 말괄량이", "부연설명"], 
+    "Kogmaw" : ["코그모", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/KogMaw_0.jpg", "KogMaw", "퉤퉤퉤퉤퉤퉤퉤퉤", "부연설명"], 
+    "Braum" : ["브라움", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Braum_0.jpg", "Braum", "듬직한 고기방패가 되어드리죠", "부연설명"], 
+    "Brand" : ["브랜드", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Brand_0.jpg", "Brand", "브랜드, 타오르는 복수", "부연설명"], 
+    "Janna" : ["잔나", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Janna_0.jpg", "Janna", "시팅", "부연설명"], 
 
-    "Braum" : ["브라움", "https://ifh.cc/g/Hie8Qc.jpg", "Braum", "듬직한 고기방패가 되어드리죠", "부연설명"], 
-    "Brand" : ["브랜드", "https://ifh.cc/g/toyi0Z.jpg", "Brand", "원딜은 쉬고있어!", "부연설명"], 
-    "Janna" : ["잔나", "https://ifh.cc/g/vFgrki.jpg", "Janna", "시팅", "부연설명"], 
-    "Bard" : ["바드", "https://ifh.cc/g/kRnasc.jpg", "Bard", "동에 번쩍 서에 번쩍", "부연설명"], 
-
-    "Kaisa" : ["카이사", "https://ifh.cc/g/4YFxPm.jpg", "Kaisa", "강하다", "부연설명"],
-    "Leona" : ["레오나", "https://ifh.cc/g/XSytqg.jpg", "Leona", "돌격!", "부연설명"],
-    "Irelia" : ["이렐리아", "https://ifh.cc/g/eqZOUK.jpg", "Irelia", "브루저의 대명사", "부연설명"],
-    "Jhin" : ["진", "https://ifh.cc/g/mDfYzO.jpg", "Jhin", "땅땅땅빵!", "부연설명"]
+    "Kaisa" : ["카이사", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kaisa_0.jpg", "Kaisa", "카이사, 공허의 딸", "부연설명"],
+    "Leona" : ["레오나", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Leona_0.jpg", "Leona", "돌격!", "부연설명"],
+    "Irelia" : ["이렐리아", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Irelia_0.jpg", "Irelia", "브루저의 대명사", "부연설명"],
+    "Jhin" : ["진", "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg", "Jhin", "땅땅땅빵!", "부연설명"]
     }
